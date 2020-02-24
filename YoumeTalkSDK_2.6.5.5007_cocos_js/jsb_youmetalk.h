1a950020db286ac4fd29136ebbae9e4a064141db
islandCw <871149538@qq.com>
2020-02-24 13:46:08 +0800

    update YoumeTalkSDK_JS

---
+++ YoumeTalkSDK_2.6.5.5007_cocos_js/jsb_youmetalk.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "base/ccConfig.h"
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT || CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID || CC_TARGET_PLATFORM == CC_PLATFORM_IOS || CC_TARGET_PLATFORM == CC_PLATFORM_MAC || CC_TARGET_PLATFORM == CC_PLATFORM_WIN32)
+
+#include "cocos/scripting/js-bindings/jswrapper/SeApi.h"
+
+extern se::Object* __jsb_YouMeTalk_proto;
+extern se::Class* __jsb_YouMeTalk_class;
+
+bool js_register_cocos2dx_extension_YouMeTalk(se::Object* obj);
+bool register_jsb_youmetalk(se::Object* obj);
+
+#endif //#if (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT || CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID || CC_TARGET_PLATFORM == CC_PLATFORM_IOS || CC_TARGET_PLATFORM == CC_PLATFORM_MAC || CC_TARGET_PLATFORM == CC_PLATFORM_WIN32)
